bfcf05dd026efddae67c373d8ab8178c47f69cb0
Wilson Desimini <wdesimini8@gmail.com>
2020-05-04 12:15:00 -0700

    create transaction using client transaction id for reversals

--- src/Entities/Transaction.ts
+++ src/Entities/Transaction.ts
@@ -58,6 +58,17 @@ export class Transaction {
     return transaction;
   }
 
+  public static fromClientId(
+    clientTransactionId: string,
+    paymentMethodType = PaymentMethodType.Credit,
+  ) {
+    const transaction = new Transaction();
+    transaction.transactionReference = new TransactionReference();
+    transaction.transactionReference.clientTransactionId = clientTransactionId;
+    transaction.transactionReference.paymentMethodType = paymentMethodType;
+    return transaction;
+  }
+
   /**
    * Allows for a follow-up request to add an additional authorization
    *
