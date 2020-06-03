4c582ec8f7e33804db0ecb7c56378162d1e1cff8
Darren Jaworski <darren.jaworski@e-hps.com>
2020-06-03 12:41:33 -0500

    HR-1205 todo

--- src/Gateways/PorticoConnector.ts
+++ src/Gateways/PorticoConnector.ts
@@ -607,6 +607,7 @@ export class PorticoConnector extends XmlGateway implements IPaymentGateway {
       subElement(Criteria, 'ClientTxnId').append(cData(trb.clientTransactionId));
       subElement(transaction, "Criteria").append(Criteria);
       transaction;
+      // todo inspect to make sure this is right structure
       debugger;
     }
 
